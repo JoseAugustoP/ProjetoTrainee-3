8cc1a77c6f5da57527be6dcc02851246c5f65dae
JoseAP <pereirajoseaugusto13@gmail.com>
2026-05-15 17:16:45 -0300

    Alteracoes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/ProjetoTrainee-3/',
   plugins: [react()],
 });
